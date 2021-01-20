fda98d3496f3da92889172e348842e73f1ffb622
pjulianyehuda <pjulianyehuda@student.ciputra.id>
2021-01-21 06:54:06 +0800

    bersih2

--- app/src/main/java/com/example/ucsatgass/util/Constants.java
+++ app/src/main/java/com/example/ucsatgass/util/Constants.java
@@ -8,10 +8,13 @@ import static java.lang.annotation.RetentionPolicy.SOURCE;
 
 public class Constants {
 
+
+    public static final String BASE_URL = "http://192.168.1.6/ucsatgas_uas/public/api/";
+
     @Retention(SOURCE)
     @StringDef
     public @interface BaseSetting {
-        String BASE_URL = "http://192.168.1.6/ucsatgas_uas/public/api/api-login";
+        String BASE_URL = "http://192.168.1.6/ucsatgas_uas/public/api/";
         String API_KEY = "";
         String BASE_IMAGE_URL = "";
         String LANGUAGE = "en-US";
